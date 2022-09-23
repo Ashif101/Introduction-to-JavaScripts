9e1634e4e3bef73b83133baef27d316c0709680c
Ashif101 <asifsaif664@gmail.com>
2022-09-23 17:05:47 +0530

    firstjava

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("p1").innerHTML = "Hello JavaScript";
+document.getElementById('p1').style.color='red';
+document.getElementById("p2").innerHTML = "This is the javascript";
+document.getElementById('p2').style.color='orange';
+document.getElementById("p3").innerHTML = "This is another  line of javascript";
+document.getElementById('p3').style.color='blue';
+document.getElementById('intro').style.color='white';
+document.getElementById('intro').style.fontSize='35px';
+document.body.style.backgroundColor  = "black";
